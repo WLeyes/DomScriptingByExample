bcb869af39823b1d71424fa32dd1511a416e9db4
Warren Leyes <warren.leyes@gmail.com>
2018-03-05 22:08:14 -0800

    cleanning up from old .js file

--- js/app.js
+++ js/app.js
@@ -1,11 +1,2 @@
-// Original from https://teamtreehouse.com/library/registering-names
+console.log('Hello from app.js');
 
-var registrar = {
-  form: document.getElementById('registrar'),
-  input: registrar.form.querySelector('input')
-};
-
-registrar.form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  console.log(input.value);
-});
